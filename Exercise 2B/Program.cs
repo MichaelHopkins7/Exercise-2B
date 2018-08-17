f562f38dbe139f5d115eba8f2949a660750b7499
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-08-17 11:51:13 -0400

    Initial Commit

--- Exercise 2B/Program.cs
+++ Exercise 2B/Program.cs
@@ -10,6 +10,35 @@ namespace Exercise_2B
     {
         static void Main(string[] args)
         {
+            // Syntactic Sugar is when a shorter, clearer, and easier to understand(for humans) 
+            //      version of syntax exist that does not decrease the functionality.
+
+
+            void MemeSound(String animal = "Doge", string sound = "WOW!")
+            {
+                Console.WriteLine($"The {animal} goes \"{sound}\"!");
+            }
+
+
+
+
+            void MemeSound(string animal, string sound)
+            {
+                Console.WriteLine($"The {animal} goes \"{sound}\"!");
+            }
+            
+            void MemeSound(string animal)
+            {
+                MemeSound(animal, "WOW!");
+            }
+            
+            void MemeSound()
+            {
+                MemeSound("Doge");
+            }
+
+
         }
+        
     }
 }
